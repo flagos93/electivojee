d4adbba8bf59b5189b22f2bf18fb7364a9674b84
Daniel San Martin <daniel.santibanez@ufscar.br>
2021-04-22 21:58:38 -0400

    Se cambio produces por consumes en automovilresource

--- src/main/java/cl/ulagos/electivojee/industriaautomotriz/boundary/AutomovilResource.java
+++ src/main/java/cl/ulagos/electivojee/industriaautomotriz/boundary/AutomovilResource.java
@@ -11,6 +11,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -60,7 +61,7 @@ public class AutomovilResource {
 	
 	@GET
 	@Path("{id}")
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Automovil obtenerAutomovil(@PathParam("id") String identificador) {
 		
 		return manufacturaAutomovil.obtenerAutomovil(identificador);
